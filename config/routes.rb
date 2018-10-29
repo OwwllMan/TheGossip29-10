efc60ca736ee4c0f01d00de687eadf955d0aed17
OwwllMan <doniermathieu@gmail.com>
2018-10-29 09:57:43 +0100

    Routes GOOD

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'statique/team'
-  get 'statique/contact'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/team' , to: 'statique#team'
+  get '/contact' , to: 'statique#contact'
 end
